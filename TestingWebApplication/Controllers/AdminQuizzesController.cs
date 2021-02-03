c67ba425a8c1aadb11bf3b9667623d5009e3e9dd
MrMouse <pakimov863@gmail.com>
2021-02-03 19:25:33 +0300

    Стилекоп

--- TestingWebApplication/Controllers/AdminQuizzesController.cs
+++ TestingWebApplication/Controllers/AdminQuizzesController.cs
@@ -8,7 +8,6 @@
     using Data.Database;
     using Data.Database.Model;
     using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
